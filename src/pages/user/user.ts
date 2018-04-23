45a1a3fc294d0051c3da260fa754a1f27370baa5
Truc Le <t5leth00@students.oamk.fi>
2018-04-23 16:22:11 +0300

    CV created, modified User pages

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -47,8 +47,8 @@ export class UserPage {
     this.navCtrl.push('ViewPostsPage');
   }
 
-  editResume() {
-    this.navCtrl.push('EditResumePage');
+  viewResume() {
+    this.navCtrl.push('ViewMyCVs');
   }
 
   logout() {
--- src/pages/view-profile/view-profile.ts
+++ src/pages/view-profile/view-profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ModalController, App } from 'ionic-angular';
 
 /**
  * Generated class for the ViewProfilePage page.
